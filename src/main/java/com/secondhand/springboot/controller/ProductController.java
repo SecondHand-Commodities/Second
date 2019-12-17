3f94dda340304adba7a46ebd9318a6c3e1019c85
renzhiwei-web <renzhiwei565@163.com>
2019-12-17 14:28:46 +0800

    商品列表展示

--- src/main/java/com/secondhand/springboot/controller/ProductController.java
+++ src/main/java/com/secondhand/springboot/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.secondhand.springboot.bean.Products;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,8 +46,9 @@ public class ProductController {
     }
 
     @RequestMapping("getAllProducts")
-    public String getProductList() {
+    public String getProductList(Model model) {
         List<Products> products = productsService.getAllProducts();
+        model.addAttribute("products",products);
         return "product-list";
     }
 }
