e0918c2d9be6a04e25bcf4666e6e95c287831694
shuhaisen <shuhaisen@shuhaisen-PC>
2019-12-18 13:29:11 +0800

    注册

--- src/main/java/com/secondhand/springboot/controller/PersonController.java
+++ src/main/java/com/secondhand/springboot/controller/PersonController.java
@@ -3,13 +3,15 @@ package com.secondhand.springboot.controller;
 import com.secondhand.springboot.Service.PersonService;
 import com.secondhand.springboot.Service.UserService;
 import com.secondhand.springboot.bean.Person;
+import com.secondhand.springboot.bean.User;
 import com.secondhand.springboot.mapper.PersonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -43,5 +45,15 @@ public class PersonController {
     public String changeInfo(Person person, HttpServletRequest request){
         personService.changeInfo(person,(String) request.getSession().getAttribute("username"));
         return "修改成功";
+    }
+    
+    @ResponseBody
+    @RequestMapping("/updatePerson")
+    public String updatePerson(Person per,HttpServletRequest request){
+    	Person person=((User) request.getSession().getAttribute("user")).getPerson();
+    	person.setAddress(per.getAddress());
+    	person.setTel(per.getTel());
+    	boolean result =personService.updatePerson(person);
+        return "处理成功";
     }
 }
